d83193315e37b7ecfddfffc2fc0fdae181e709da
djedi-knight <shawn.daichendt@gmail.com>
2021-06-21 14:49:27 -0400

    Implemented customerStorage

--- src/main/kotlin/models/Customer.kt
+++ src/main/kotlin/models/Customer.kt
@@ -2,5 +2,8 @@ package models
 
 import kotlinx.serialization.Serializable
 
+// In-memory storage
+val customerStorage = mutableListOf<Customer>()
+
 @Serializable
 data class Customer(val id: String, val firstName: String, val lastName: String, val email: String)
