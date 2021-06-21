6999ccb83985968afc0ee954350b03a56ba5cb8a
djedi-knight <shawn.daichendt@gmail.com>
2021-06-21 15:09:55 -0400

    Created GET API for specified Customer

--- src/main/kotlin/com/jetbrains/handson/httpapi/routes/CustomerRoutes.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/routes/CustomerRoutes.kt
@@ -9,14 +9,28 @@ import io.ktor.routing.*
 fun Route.customerRouting() {
     route("/customer") {
         get {
+            // Check if Customers exist
             if (customerStorage.isNotEmpty()) {
+                // Return all Customers
                 call.respond(customerStorage)
             } else {
+                // Return error message
                 call.respondText("No customers found", status = HttpStatusCode.NotFound)
             }
         }
         get("{id}") {
-
+            // Get ID (check if it exists in the request)
+            val id = call.parameters["id"] ?: return@get call.respondText(
+                "Missing or malformed id",
+                status = HttpStatusCode.BadRequest
+            )
+            // Get customer (check if it exists)
+            val customer =
+                customerStorage.find { it.id == id } ?: return@get call.respondText(
+                    "No customer with id $id",
+                    status = HttpStatusCode.NotFound
+                )
+            call.respond(customer)
         }
         post {
 
