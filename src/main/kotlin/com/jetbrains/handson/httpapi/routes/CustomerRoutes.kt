9d6264be052e00ed197c816d53353586c30762ad
djedi-knight <shawn.daichendt@gmail.com>
2021-06-21 15:16:06 -0400

    Created POST API call for Customer

--- src/main/kotlin/com/jetbrains/handson/httpapi/routes/CustomerRoutes.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/routes/CustomerRoutes.kt
@@ -1,8 +1,10 @@
 package com.jetbrains.handson.httpapi.routes
 
+import com.jetbrains.handson.httpapi.models.Customer
 import com.jetbrains.handson.httpapi.models.customerStorage
 import io.ktor.application.*
 import io.ktor.http.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
@@ -20,21 +22,28 @@ fun Route.customerRouting() {
         }
         get("{id}") {
             // Get ID (check if it exists in the request)
+            // Note: this will never happen, since this route will only be called if ID is present
             val id = call.parameters["id"] ?: return@get call.respondText(
                 "Missing or malformed id",
                 status = HttpStatusCode.BadRequest
             )
-            // Get customer (check if it exists)
+            // Get Customer (check if it exists)
             val customer =
                 customerStorage.find { it.id == id } ?: return@get call.respondText(
                     "No customer with id $id",
                     status = HttpStatusCode.NotFound
                 )
+            // Return Customer
             call.respond(customer)
         }
         post {
-
+            // Add Customer to storage
+            val customer = call.receive<Customer>()
+            customerStorage.add(customer)
+            // Return success message
+            call.respondText("Customer stored correctly", status = HttpStatusCode.Created)
         }
+
         delete("{id}") {
 
         }
