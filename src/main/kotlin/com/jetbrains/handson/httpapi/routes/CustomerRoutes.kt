29e01337da235c6f73dc76da8e6710a54dcaf6d7
djedi-knight <shawn.daichendt@gmail.com>
2021-06-21 14:52:25 -0400

    Created CustomerRoutes

---
+++ src/main/kotlin/com/jetbrains/handson/httpapi/routes/CustomerRoutes.kt
@@ -0,0 +1,20 @@
+package com.jetbrains.handson.httpapi.routes
+
+import io.ktor.routing.*
+
+fun Route.customerRouting() {
+    route("/customer") {
+        get {
+
+        }
+        get("{id}") {
+
+        }
+        post {
+
+        }
+        delete("{id}") {
+
+        }
+    }
+}
