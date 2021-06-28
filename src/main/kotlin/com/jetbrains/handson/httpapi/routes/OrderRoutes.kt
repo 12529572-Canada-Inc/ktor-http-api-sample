b349abd8c1efd075e4779b1c4469188ab61ee8b4
djedi-knight <shawn.daichendt@gmail.com>
2021-06-27 23:18:39 -0400

    Created getOrderRoute

--- src/main/kotlin/com/jetbrains/handson/httpapi/routes/OrderRoutes.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/routes/OrderRoutes.kt
@@ -2,6 +2,7 @@ package com.jetbrains.handson.httpapi.routes
 
 import com.jetbrains.handson.httpapi.models.orderStorage
 import io.ktor.application.*
+import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
@@ -10,5 +11,16 @@ fun Route.listOrdersRoute() {
         if (orderStorage.isNotEmpty()) {
             call.respond(orderStorage)
         }
+    }
+}
+
+fun Route.getOrderRoute() {
+    get("/order/{id}") {
+        val id = call.parameters["id"] ?: return@get call.respondText("Bad Request", status = HttpStatusCode.BadRequest)
+        val order = orderStorage.find { it.number == id } ?: return@get call.respondText(
+            "Not Found",
+            status = HttpStatusCode.NotFound
+        )
+        call.respond(order)
     }
 }
